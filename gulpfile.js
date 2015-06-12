13927acbcdfcb75bca3c01e78ea5a9057cca6654
Rasmus Eskola <fruitiex@gmail.com>
2015-06-12 20:20:50 +0300

    inject a custom js file

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var inject = require('gulp-inject');
 var bowerFiles = require('main-bower-files');
 
 gulp.task('default', function() {
-    gulp.src('./public_src/index.html')
-        .pipe(inject(gulp.src(bowerFiles(), {read: false}, {name: 'bower', relative: false})))
-        .pipe(inject(gulp.src('./public/js/*.js', {read: false, relative: false})))
+    gulp.src('./app/index.html')
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+        .pipe(inject(gulp.src(['./js/*.js'], {read: false})))
         .pipe(gulp.dest('./public'))
 });
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/js', express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
