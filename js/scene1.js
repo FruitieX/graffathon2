ef3fc0071a62b95e2e412a828b81d39b896ce751
Markus Palonen <markus.palonen@aalto.fi>
2015-06-12 22:22:07 +0300

    Scene 1 edit

--- js/scene1.js
+++ js/scene1.js
@@ -1,19 +1,36 @@
 function Scene1() {
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    this.cube = new THREE.Mesh( geometry, material );
+    var geometry = new THREE.IcosahedronGeometry(2, 0);
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffdd, specular: 0x009900, emissive: 0x006063, shininess: 50} );
+    this.obj = new THREE.Mesh( geometry, material );
+    speed = 0.01;
 };
 
 Scene1.prototype.init = function() {
+
+    // Add objects
     curThreeScene = new THREE.Scene();
-    camera.position.z = 5;
-    curThreeScene.add(this.cube);
+    camera.position.z = 10;
+    curThreeScene.add(this.obj);
+ 
+    // Add ambient lighting
+    var ambientLight = new THREE.AmbientLight(0x333333);
+    scene.add(ambientLight);
+
+    // Add directional lightning
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    directionalLight.position.set( 0, 1, 1 );
+    scene.add( directionalLight );
 };
 
 Scene1.prototype.deinit = function() {
 };
 
 Scene1.prototype.update = function(dt) {
-    this.cube.rotation.x += 0.1;
-    this.cube.rotation.y += 0.1;
+    this.obj.rotation.x += 0.05;
+    this.obj.rotation.y += 0.05;
+    if (camera.position.z < 3)
+        speed *= -1;
+    else if (camera.position.z > 15)
+        speed *= -1;
+    camera.position.z += speed*10;
 };
