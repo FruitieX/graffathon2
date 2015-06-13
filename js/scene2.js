27ad846fc5de6ca62bf56fcd715c245df36acc92
unknown <atte.seppala@gmail.com>
2015-06-13 11:25:51 +0300

    things

--- js/scene2.js
+++ js/scene2.js
@@ -1,11 +1,11 @@
 function Scene2() {
     console.log("asd");
-    this.numParticles = 7;
+    this.numParticles = 19;
     this.gravityCenter = new THREE.Vector3(0, 0, 0);
-    this.gravityConstant = 0.000001;
+    this.gravityConstant = 0.0000001;
     this.dx = [];
     this.d2x = [];
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < this.numParticles; i++) {
         this.dx.push(new THREE.Vector3(0,0,0));
         this.d2x.push(new THREE.Vector3(0,0,0));
     }
@@ -19,10 +19,12 @@ Scene2.prototype.init = function() {
 
     var particleGeometry = new THREE.Geometry();
     for (var i = 0; i < this.numParticles; i++) {
-        particleGeometry.vertices.push(new THREE.Vector3(i * Math.random(), i, Math.random()));
+        particleGeometry.vertices.push(new THREE.Vector3((Math.random() - 0.5)*100, (Math.random()-0.5)*100, (Math.random()-0.5)*100));
+        particleGeometry.vertexColors[i] = 0xFFFFFF * Math.random();
     }
     var particleMaterial = new THREE.PointCloudMaterial({
-        size: 1
+        size: 1,
+        vertexColors:THREE.VertexColors
     });
     this.particles = new THREE.PointCloud(particleGeometry, particleMaterial);
 
@@ -45,8 +47,16 @@ Scene2.prototype.update = function(dt, t) {
         }
         var diff = this.gravityCenter.clone().sub(this.particles.geometry.vertices[i]);
         this.d2x[i] = diff.multiplyScalar(this.gravityConstant * (1- Math.random() * 0.1));
+        this.d2x[i].z += (Math.random() - 0.5) * 0.1 * this.gravityConstant;
     }
-    this.gravityCenter.x = Math.sin(audio.get(0).currentTime * 2);
-    this.gravityCenter.y = Math.cos(audio.get(0).currentTime * 2);
+    var time = audio.get(0).currentTime * 1000;
+    this.gravityCenter.x = Math.sin(time * 0.1) * 10;
+    this.gravityCenter.y = Math.cos(time * 0.1) * 10;
+    var timeMod = time % 2000;
+    if (timeMod > 1000) {
+        this.gravityConstant = -0.000001;
+    } else{
+        this.gravityConstant = 0.000001;
+    }
 
 };
